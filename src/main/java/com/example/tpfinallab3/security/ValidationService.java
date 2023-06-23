b26aa88dd76c1b13ca6578fd57e38f7d1139e904
Federico Gil de Muro <federicogildemuro@gmail.com>
2023-06-23 15:04:58 -0300

    corregidas validaciones nuevo paciente

--- src/main/java/com/example/tpfinallab3/security/ValidationService.java
+++ src/main/java/com/example/tpfinallab3/security/ValidationService.java
@@ -1,6 +1,5 @@
 package com.example.tpfinallab3.security;
 
-import com.example.tpfinallab3.exceptions.ContrasenaException;
 import com.example.tpfinallab3.exceptions.NombreUsuarioException;
 import com.example.tpfinallab3.exceptions.ValidationException;
 import com.example.tpfinallab3.models.Paciente;
@@ -23,57 +22,53 @@ public class ValidationService {
         return instance;
     }
 
-    public void validarDatosNuevoUsuario(Usuario usuario) throws NombreUsuarioException, ContrasenaException, ValidationException {
+    public void validarDatosNuevoUsuario(Usuario usuario) throws NombreUsuarioException, ValidationException {
 
         //si existe registrado el nombre de usuario
         if(AuthenticationService.getInstance().usuarioExiste(usuario.getNombreUsuario())) {
             throw new NombreUsuarioException("Ya existe un usuario registrado con el mismo nombre");
         }
 
-        //si el nombre de usuario tiene números o caracteres especiales
-        if(!usuario.getNombreUsuario().matches("[a-zA-Z]*")) {
-            throw new NombreUsuarioException("Nombre de usuario no puede contener números ni caracteres especiales");
-        }
-
-        //si la contraseña es demasiado corta
-        if(usuario.getContrasena().length() >= 1 && usuario.getContrasena().length() < 6) {
-            throw new ContrasenaException("Contraseña demasiado corta (mínimo 6 caracteres)");
-        }
-
         //mensaje en el que se van a acumular distintos errores en el ingreso de datos
         StringBuilder mensaje = new StringBuilder();
 
-        //si el nombre de usuario está vacío
+        //si el nombre de usuario está vacío y si tiene números o caracteres especiales
         if(usuario.getNombreUsuario().isEmpty()) {
-            mensaje.append("Nombre de usuario obligatorio\n");
+            mensaje.append("Usuario obligatorio\n");
+        }
+        else if(usuario.getNombreUsuario().matches("[^a-zA-Z]*")) {
+            mensaje.append("Usuario no puede contener números ni caracteres especiales\n");
         }
 
-        //si la contraseña está vacía
+        //si la contraseña está vacía y si es demasiado corta
         if(usuario.getContrasena().isEmpty()) {
             mensaje.append("Contraseña obligatoria\n");
+        }
+        else if(usuario.getContrasena().length() >= 1 && usuario.getContrasena().length() < 6) {
+            mensaje.append("Contraseña demasiado corta (mínimo 6 caracteres)\n");
         }
 
         //si el nombre está vacío y si tiene números
         if(usuario.getNombre().isEmpty()) {
             mensaje.append("Nombre obligatorio\n");
         }
-        else if (usuario.getNombre().matches("[0-9]+")) {
-            mensaje.append("Nombre no puede contener números ni caracteres especiales\n");
+        else if(usuario.getNombre().matches("^\\d")) {
+            mensaje.append("Nombre no puede contener números\n");
         }
 
         //si el apellido está vacío y si tiene números
         if(usuario.getApellido().isEmpty()) {
             mensaje.append("Apellido obligatorio\n");
         }
-        else if (usuario.getApellido().matches("[0-9]+")) {
-            mensaje.append("Apellido no puede contener números ni caracteres especiales\n");
+        else if(usuario.getApellido().matches("^\\d")) {
+            mensaje.append("Apellido no puede contener números\n");
         }
 
-        //si el mail está vacío y si no tiene arroba y punto
+        //si el mail está vacío y si tiene formato válido
         if(usuario.getMail().isEmpty()) {
             mensaje.append("Correo electrónico obligatorio\n");
         }
-        else if(!usuario.getContrasena().contains("@") || !usuario.getContrasena().contains(".")) {
+        else if(!usuario.getMail().matches("[\\w-]+(\\.[\\w-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")) {
             mensaje.append("Correo electrónico inválido\n");
         }
 
@@ -84,20 +79,19 @@ public class ValidationService {
             //si el dni está vacío y si tiene caracteres que no sean números
             if(paciente.getDni().isEmpty()) {
                 mensaje.append("Número de documento obligatorio\n");
-            } else if (!paciente.getDni().matches("[0-9]+")) {
+            } else if (!paciente.getDni().matches("\\d")) {
                 mensaje.append("Número de documento inválido\n");
             }
 
             //si teléfono tiene caracteres distintos de números
-            if(!paciente.getTelefono().isEmpty() && !paciente.getTelefono().matches("[0-9]+")) {
+            if(!paciente.getTelefono().isEmpty() && !paciente.getTelefono().matches("\\d")) {
                 mensaje.append("Teléfono inválido");
             }
         }
 
         //si el mensaje no está vacío es porque se detectó algún error en el ingreso de datos y se lanza excepción
-        if(!mensaje.isEmpty()) {
+        if(!mensaje.toString().isEmpty()) {
             throw new ValidationException (mensaje.toString());
         }
     }
 }
-
