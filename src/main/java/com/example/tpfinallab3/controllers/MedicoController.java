103a935bd74ee95c9dcc5e6ae0e20be13030b0a0
maru33luc <maru33luc@gmail.com>
2023-06-26 13:34:25 -0300

    :sparkles: Medico ve todos sus turnos

--- src/main/java/com/example/tpfinallab3/controllers/MedicoController.java
+++ src/main/java/com/example/tpfinallab3/controllers/MedicoController.java
@@ -154,9 +154,17 @@ public class MedicoController {
                 tablaMedicoColumnaHora.setCellValueFactory(new PropertyValueFactory("hora"));
                 tablaMedicoColumnaPaciente.setCellValueFactory(new PropertyValueFactory("paciente"));
 
-                tablaTurnosMedico.setItems(FXCollections.observableArrayList(listaTurnosTabla));
+
+            }else {
+                TurnoTablaMedico turnoTablaMedico = new TurnoTablaMedico(turno.getDia(), turno.getHora(), null);
+                listaTurnosTabla.add(turnoTablaMedico);
+                tablaTurnoMedicoColumnaFecha.setCellValueFactory(new PropertyValueFactory("dia"));
+                tablaMedicoColumnaHora.setCellValueFactory(new PropertyValueFactory("hora"));
+                tablaMedicoColumnaPaciente.setCellValueFactory(new PropertyValueFactory("paciente"));
             }
         }
+        tablaTurnosMedico.setItems(FXCollections.observableArrayList(listaTurnosTabla));
+
     }
     @FXML
     private void verPerfilMedico(MouseEvent event) {
